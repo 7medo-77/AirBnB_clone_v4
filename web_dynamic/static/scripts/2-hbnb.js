74d62532a8e8bb81e4f5a61684e9f964cafc5973
hmedo <hmedo@7medo>
2024-06-03 14:04:47 +0300

    web_dynamic_14-04

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,12 +1,15 @@
 $(document).ready(() => {
-	$.get(() => {
+	$.get({
 		type: 'GET',
-		url: 'http://0.0.0.0:5001/api/v1/status/',
+		// url: 'http://0.0.0.0:5001/api/v1/status/',
+		url: 'http://127.0.0.1:5001/api/v1/status/',
 		success: (data) => {
-			if (data.status === "OK") {
+			if (data["status"] === "OK") {
 				$('#api_status').addClass("available");
+				$('#api_status').css("background-color", "#ff545f");
 			} else {
 				$('#api_status').removeClass("available");
+				$('#api_status').css("background-color", "#cccccc");
 			}
 		},
 	})
